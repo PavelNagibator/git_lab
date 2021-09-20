8c9d465c28ae7acddf199639d1563d01f16f128e
study <study@lan.sibadi.org>
2021-09-20 18:24:16 +0600

    change code

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Исходная версия");
+            Console.WriteLine("Исходная версия/// а теперь я его поменял");
             Console.ReadKey();
         }
     }
